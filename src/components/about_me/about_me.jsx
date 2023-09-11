e3373a6de984183e47fadfe9c6251fbd7afa5772
Parker-Pond <parker.a.pond@gmail.com>
2023-09-11 10:14:09 -0600

    about me added

--- src/components/about_me/about_me.jsx
+++ src/components/about_me/about_me.jsx
@@ -1,8 +1,56 @@
+import "./about_me.css"
 
-
-export default function About_me (){
-
-    return (<>
-    <h1>about me</h1>
-    </>)
+export default function About_me() {
+  return (
+    <>
+      <h1>about me</h1>
+      <div className="text_box">
+        <p>
+          Raised in Las Vegas and I am currently living in Provo Utah with my
+          beautiful wife and a child on the way. We love living so close to the
+          mountains and trails, the food here is also excellent, with the local
+          restaurant and farmers market scenes. (I might be a foody, but that
+          also comes with me bringing in baked goods to work, so… you have been
+          warned.)
+        </p>
+        <p>
+          From a young age I was taught the value of effort and how to run a
+          business. These skills tend to get me moved to management positions,
+          in which I do very well, but I am just as content working under
+          someone and in a team. Years of customers service, sales, and general
+          extravertedness has given me a skill for figuring out what a customer
+          needs and how to get them the closes thing. This has given me an
+          ability to work with difficult and angry customers.
+        </p>
+        <p>
+          Over the years of sales, food, and applied behavior analytics (quite a
+          different field I know), I have developed these skills and am very
+          good at them, but just because you are good at something does not mean
+          you enjoy doing said thing. I have been working on changing careers
+          but figuring out which one has been hard, but then I remembered the
+          love I had for computer programming.
+        </p>
+        <p>
+          I found computer programming in high school where I joined a club and
+          fell in love. After a year the club disbanded and school took over,
+          but I always remembered how much I loved it. In May of 2023 I decided
+          to take the plunge and try my hand at web development, and I fell in
+          love.
+        </p>
+        <p>
+          I attended Fullstack academies’ full stack development course and
+          loved every minute of it. My free time was full of learning new things
+          and I drank in the lessons we had. I had forgotten what it was like to
+          love what you do, not just be so good at something that people
+          automatically thought you must love it.
+        </p>
+        <p>
+          I pride myself on being a lifetime learner and the tech industry is
+          perfect for that, as it is constantly evolving, I am never going to
+          run out of new things to learn, and even things I do know are going to
+          evolve so I can learn more on them.
+        </p>
+      </div>
+    </>
+  );
 }
